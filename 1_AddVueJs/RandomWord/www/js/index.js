b1f2f935b8445e9ea7a19e5d6fa5a154702e0598
Fady <fmakram1@gmail.com>
2016-08-28 10:55:50 -0400

    initial commit

---
+++ 1_AddVueJs/RandomWord/www/js/index.js
@@ -0,0 +1,38 @@
+var app = { 
+    initialize: function() { 
+        this.bindEvents(); 
+        this.setupVue();
+    }, 
+    bindEvents: function() { 
+        document.addEventListener('deviceready', this.onDeviceReady, false); 
+    }, 
+    onDeviceReady: function() { 
+        app.receivedEvent('deviceready'); 
+    }, 
+    receivedEvent: function(id) { 
+        console.log('Received Event: ' + id); 
+    }, 
+    setupVue: function() { 
+        var vm = new Vue({ 
+            el: "#vue-instance",
+            data: {
+                randomWord: '',
+                words: [
+                    'formidable',
+                    'gracious',
+                    'daft',
+                    'mundane',
+                    'onomatopoeia'
+                ]
+            },
+            methods: {
+                getRandomWord: function() {
+                    var randomIndex = Math.floor(Math.random() * this.words.length);
+                    this.randomWord = this.words[randomIndex]
+                }
+            }
+        }); 
+    } 
+}; 
+ 
+app.initialize();
---
+++ 2_AddHttpRequest/RandomWord/www/js/index.js
@@ -0,0 +1,37 @@
+var app = { 
+    initialize: function() { 
+        this.bindEvents(); 
+        this.setupVue();
+    }, 
+    bindEvents: function() { 
+        document.addEventListener('deviceready', this.onDeviceReady, false); 
+    }, 
+    onDeviceReady: function() { 
+        app.receivedEvent('deviceready'); 
+    }, 
+    receivedEvent: function(id) { 
+        console.log('Received Event: ' + id); 
+    }, 
+    setupVue: function() { 
+        var vm = new Vue({ 
+            el: "#vue-instance",
+            data: {
+                randomWord: ''
+            },
+            methods: {
+                getRandomWord: function() {
+                    this.randomWord = '...';
+                    this.$http.get(
+                        'http://api.wordnik.com:80/v4/words.json/randomWord?api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5'
+                    ).then(function (response) {
+                        this.randomWord = response.data.word;
+                    }, function (error) {
+                        alert(error.data);
+                    });
+                }
+            }
+        }); 
+    } 
+}; 
+
+app.initialize();
